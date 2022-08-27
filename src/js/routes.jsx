70d1b3ed49d29bcd8be8bc0e0cb5385e48c0ea3a
Mathew Joe Thomas <mathew.j@keyvalue.systems>
2022-08-28 01:38:14 +0530

    design changes

--- src/js/routes.jsx
+++ src/js/routes.jsx
@@ -2,27 +2,16 @@ import React from 'react';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
 
 import HomePage from './containers/home-page';
+import HomePage2 from './containers/ticketList';
 import TicketListing from './containers/ticketListing';
 
 export default (
   <div>
-    <div>
-      <Link className="link" to="/home" style={{ textDecoration: 'none' }}>Home</Link>
-      <Link
-        className="link"
-        style={{
-          fontSize: '17px',
-          textDecoration: 'none',
-          marginLeft: '20px'
-        }}
-        to="/ticket-listing">
-        My Bookings
-      </Link>
-    </div>
     <Route>
       <Switch>
         <Redirect from="/" to="/home" exact={true} />
         <Route path="/home" component={HomePage} exact={true} />
+        <Route path="/home2" component={HomePage2} exact={true} />
         <Route path="/ticket-listing" component={TicketListing} exact={true} />
         <Route path="/" component={HomePage} exact={true} />
       </Switch>
