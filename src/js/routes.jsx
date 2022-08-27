80e14c80cd5a1b7e0dc7d34f243d26a353979aec
adithya-vinod <adithya3330@gmail.com>
2022-08-27 22:57:47 +0530

    Ticket Details Card Completed

--- src/js/routes.jsx
+++ src/js/routes.jsx
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect, Link } from 'react-router-dom';
 
 import HomePage from './containers/home-page';
 import TicketListing from './containers/ticketListing';
+import TicketDetails from './containers/ticketDetails/ticketDetails';
 
 export default (
   <div>
@@ -18,12 +19,23 @@ export default (
         to="/ticket-listing">
         My Bookings
       </Link>
+      <Link
+        className="link"
+        style={{
+          fontSize: '17px',
+          textDecoration: 'none',
+          marginLeft: '20px'
+        }}
+        to="/ticket-details">
+        Ticket Details
+      </Link>
     </div>
     <Route>
       <Switch>
         <Redirect from="/" to="/home" exact={true} />
         <Route path="/home" component={HomePage} exact={true} />
         <Route path="/ticket-listing" component={TicketListing} exact={true} />
+        <Route path="/ticket-details" component={TicketDetails} exact={true} />
         <Route path="/" component={HomePage} exact={true} />
       </Switch>
     </Route>
